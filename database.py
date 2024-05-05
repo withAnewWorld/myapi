fc63657b43364543e3e5b57ab0631d45100c2b41
jhyeok <ljhljh6887@naver.com>
2024-05-05 06:44:52 +0000

    .env 파일을 사용하도록 수정

--- database.py
+++ database.py
@@ -1,7 +1,11 @@
 from sqlalchemy import create_engine 
 from sqlalchemy.orm import sessionmaker
+from starlette.config import Config
 
 SQLALCHEMY_DATABASE_URL = "sqlite:///./myapi.db"
+
+config =  Config('.env')
+SQLALCHEMY_DATABASE_URL = config('SQLACHEMY_DATABASE_URL')
 
 engine = create_engine(
   SQLALCHEMY_DATABASE_URL, connect_args = {"check_same_thread": False}
